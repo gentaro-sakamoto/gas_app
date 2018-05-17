d73974189a29e5ecea2e692c5c2b6d7ec001cf20
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2018-05-17 15:48:10 +0900

    Allow to export dogRuns as JSON

--- dog_runs/js/main.js
+++ dog_runs/js/main.js
@@ -1,1 +1,15 @@
-global.main = () => {};
+import TableFactory from './services/table_factory';
+
+global.doGet = () => {
+  const table = new TableFactory('dog_runs');
+  const headers = table.getHeader();
+
+  let dogRun;
+  const dogRuns = table.items.map(row => {
+    dogRun = {};
+    headers.forEach(key => dogRun[key] = row.getFieldValue(key));
+    return dogRun;
+  });
+  const dogRunsAsJson = JSON.stringify(dogRuns);
+  return ContentService.createTextOutput(dogRunsAsJson).setMimeType(ContentService.MimeType.JAVASCRIPT);
+};
