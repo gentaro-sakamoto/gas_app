2ddfc251221f4742de3030c82a298b18658ada71
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2018-05-21 13:37:40 +0900

    Allow to limit items.

--- dog_runs/js/main.js
+++ dog_runs/js/main.js
@@ -1,11 +1,12 @@
 import TableFactory from './services/table_factory';
 
-global.doGet = () => {
+global.doGet = (e) => {
+  const limit = e.parameter.limit || 10;
   const table = new TableFactory('dog_runs');
   const headers = table.getHeader();
 
   let dogRun;
-  const dogRuns = table.items.map(row => {
+  const dogRuns = table.items.slice(0, limit).map(row => {
     dogRun = {};
     headers.forEach(key => dogRun[key] = row.getFieldValue(key));
     return dogRun;
