1c34a4930b06e6322eec97cfbd3188ea9eaf5591
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2018-05-25 14:47:39 +0900

    Allow to filter by id.

--- dog_runs/js/main.js
+++ dog_runs/js/main.js
@@ -1,16 +1,26 @@
 import TableFactory from './services/table_factory';
 
 global.doGet = (e) => {
+  const id = e.parameter.id || null;
   const limit = e.parameter.limit || 10;
   const table = new TableFactory('dog_runs');
   const headers = table.getHeader();
+  let dogRun = {};
+  let data = null;
 
-  let dogRun;
-  const dogRuns = table.items.slice(0, limit).map(row => {
-    dogRun = {};
+  if (id) {
+    let row = table.select({ 'id': parseInt(id) }).first();
     headers.forEach(key => dogRun[key] = row.getFieldValue(key));
-    return dogRun;
-  });
-  const dogRunsAsJson = JSON.stringify(dogRuns);
+    data = dogRun;
+  } else {
+    const dogRuns = table.items.slice(0, limit).map(row => {
+      dogRun = {};
+      headers.forEach(key => dogRun[key] = row.getFieldValue(key));
+      return dogRun;
+    });
+    data = dogRuns;
+  }
+
+  const dogRunsAsJson = JSON.stringify(data);
   return ContentService.createTextOutput(dogRunsAsJson).setMimeType(ContentService.MimeType.JAVASCRIPT);
 };
