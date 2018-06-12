27f4686fc30a0640703916762cb17b1caae34060
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2018-06-12 12:05:13 +0900

    Export dog-runs-v2 as json

--- dog_runs/js/main.js
+++ dog_runs/js/main.js
@@ -3,7 +3,7 @@ import TableFactory from './services/table_factory';
 global.doGet = (e) => {
   const id = e.parameter.id || null;
   const limit = e.parameter.limit || 10;
-  const table = new TableFactory('dog_runs');
+  const table = new TableFactory('dog-runs-v2');
   const headers = table.getHeader();
   let dogRun = {};
   let data = null;
@@ -13,9 +13,13 @@ global.doGet = (e) => {
     headers.forEach(key => dogRun[key] = row.getFieldValue(key));
     data = dogRun;
   } else {
-    const dogRuns = table.items.slice(0, limit).map(row => {
+    const dogRuns = table.select({present: '存在'}).slice(0, limit).map(row => {
       dogRun = {};
-      headers.forEach(key => dogRun[key] = row.getFieldValue(key));
+      headers.forEach(key => {
+        if (key != 'present') {
+          dogRun[key] = row.getFieldValue(key)
+        }
+      });
       return dogRun;
     });
     data = dogRuns;
